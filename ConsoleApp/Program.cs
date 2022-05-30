d89a6cdca3f68ba1c7fb1408f8cb3544166dfe53
Paweł Łęczycki <pawel@msd.info.pl>
2022-05-30 10:22:27 +0200

    Konfiguracja z pliku

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,1 +1,26 @@
-﻿Console.WriteLine("Hello!");
+﻿
+using Microsoft.Extensions.Configuration;
+
+//Microsoft.Extensions.Configuration
+var config = new ConfigurationBuilder()
+            //package Microsoft.Extensions.Configuration.FileExtensions
+            //package Microsoft.Extensions.Configuration.Xml
+            .AddXmlFile("Configuration/config.xml", optional: true)
+            //package Microsoft.Extensions.Configuration.Json
+            .AddJsonFile("Configuration/config.json", optional: false, reloadOnChange: true)
+            //package Microsoft.Extensions.Configuration.ini
+            .AddIniFile("Configuration/config.ini", optional: true)
+            //package NetEscapades.Configuration.Yaml
+            .AddYamlFile("Configuration/config.yaml", optional: true)
+            .Build();
+
+
+//while (true)
+{
+    Console.WriteLine($"Hello {config["HelloJson"]}");
+    Console.WriteLine($"Hello {config["HelloXml"]}");
+    Console.WriteLine($"Hello {config["HelloIni"]}");
+    Console.WriteLine($"Hello {config["HelloYaml"]}");
+    Console.WriteLine($"{config["Bye"]}");
+    Thread.Sleep(1000);
+}
